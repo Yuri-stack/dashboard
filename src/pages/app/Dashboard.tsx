950315746f6c64b2f8e5f8811c9a611ce5418c92
Yuri-stack <yurioliveirasilva1999@gmail.com>
2025-07-02 18:46:20 -0300

    bo resolvido

--- src/pages/app/Dashboard.tsx
+++ src/pages/app/Dashboard.tsx
@@ -6,7 +6,7 @@ export function Dashboard() {
         <>
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
                 <Summary />
-                {/* <BalanceChart /> */}
+                <BalanceChart />
             </div>
         </>
     )
